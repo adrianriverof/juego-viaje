1a4bb96998e61ff5ea29b75e3ccf35583717c8df
adrianriverof <adrianriverof@gmail.com>
2023-12-06 11:57:07 +0100

    sistema de avance preliminar

--- player.gd
+++ player.gd
@@ -4,7 +4,9 @@ extends KinematicBody2D
 var cantidad_giro = 0 
 var cantidad_avance = 0 
 
+var velocity  = Vector2.ZERO
 
+var speed = 500
 
 func _physics_process(delta):
 	move()
@@ -20,6 +22,15 @@ func move():
 	
 func avance():
 	avance_inputs()
+	
+	
+	
+	velocity.x = lerp(velocity.x, cantidad_avance * speed * cos(rotation), 0.2)
+	velocity.y = lerp(velocity.y, cantidad_avance * speed * sin(rotation), 0.2)
+	
+	move_and_slide(velocity)
+
+	
 
 func avance_inputs():
 	cantidad_avance = int(Input.is_action_pressed("p1up")) - int(Input.is_action_pressed("p1down"))
