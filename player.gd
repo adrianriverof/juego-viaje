a8778ac46814d2e5b9a5062abeafa71aed3906ee
adrianriverof <adrianriverof@gmail.com>
2023-12-06 12:04:45 +0100

    sacar variables ajustables

--- player.gd
+++ player.gd
@@ -6,27 +6,32 @@ var cantidad_avance = 0
 
 var velocity  = Vector2.ZERO
 
+var rotation_radians = 0.5
 var speed = 500
+var move_weight = 0.05
+
 
 func _physics_process(delta):
 	move()
 	
 
 
-func move():
-	#acceleration()
-	
+
+
+
+
+func move():	
 	avance()
 	giro()
-	
-	
+
+
 func avance():
 	avance_inputs()
 	aplicar_velocidad_segun_angulo()
 	
 
 func aplicar_velocidad_segun_angulo():
-	var move_weight = 0.05
+	
 	velocity.x = lerp(velocity.x, cantidad_avance * speed * cos(rotation), move_weight)
 	velocity.y = lerp(velocity.y, cantidad_avance * speed * sin(rotation), move_weight)
 	
@@ -35,18 +40,16 @@ func aplicar_velocidad_segun_angulo():
 func avance_inputs():
 	cantidad_avance = int(Input.is_action_pressed("p1up")) - int(Input.is_action_pressed("p1down"))
 
-
 func giro():
 	giro_inputs()
 	rotar_todo()
-	
-	
+
 func rotar_todo():
-	rotation += cantidad_giro * 0.5/(2 * PI)
+	rotation += cantidad_giro * rotation_radians/(2 * PI)
 
 func giro_inputs():
 	cantidad_giro = int(Input.is_action_pressed("p1right")) - int(Input.is_action_pressed("p1left"))
-	#print(cantidad_giro)
+	
 
 
 
