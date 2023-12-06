ee242a32ad08d30e9117feba787a66939630d160
adrianriverof <adrianriverof@gmail.com>
2023-12-06 12:41:15 +0100

    refactor bullet

--- bullet.gd
+++ bullet.gd
@@ -9,12 +9,21 @@ var velocity = Vector2(0,0)
 
 
 func _ready():
+	rota_la_direccion()
+	aplica_inercia_y_velocidad()
+	rotacion_general()
 	
+	
+func rota_la_direccion():
 	direction_vect = direction_vect.rotated(direction)
-	
+
+func aplica_inercia_y_velocidad():
 	velocity = inertia + direction_vect * speed
+
+func rotacion_general():
 	rotation = direction
 	
+
 func _physics_process(delta):
 	translate(velocity)
 	
