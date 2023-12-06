b8329eeeb1b21adba7e841dab4f687cbf8bc7ed1
adrianriverof <adrianriverof@gmail.com>
2023-12-06 11:50:08 +0100

    avance inputs

--- player.gd
+++ player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 
-var cantidad_giro = 0
+var cantidad_giro = 0 
+var cantidad_avance = 0 
 
 
 
@@ -12,8 +13,18 @@ func _physics_process(delta):
 
 func move():
 	#acceleration()
+	
+	avance()
 	giro()
 	
+	
+func avance():
+	avance_inputs()
+
+func avance_inputs():
+	cantidad_avance = int(Input.is_action_pressed("p1up")) - int(Input.is_action_pressed("p1down"))
+
+
 func giro():
 	giro_inputs()
 	rotar_todo()
@@ -21,9 +32,6 @@ func giro():
 	
 func rotar_todo():
 	rotation += cantidad_giro * 0.5/(2 * PI)
-	
-	
-
 
 func giro_inputs():
 	cantidad_giro = int(Input.is_action_pressed("p1right")) - int(Input.is_action_pressed("p1left"))
