9fd815d9fbebc08b92960491cb1f6b505e01a816
adrianriverof <adrianriverof@gmail.com>
2023-12-06 12:37:43 +0100

    disparo funcional

--- bullet.gd
+++ bullet.gd
@@ -2,15 +2,19 @@ extends Area2D
 
 
 var direction
-var direction_vect = Vector2(0,0)
-var speed = 20
+var direction_vect = Vector2(1,0)
+var speed = 10
 var inertia = Vector2(0,0)
 var velocity = Vector2(0,0)
 
 
 func _ready():
+	
+	direction_vect = direction_vect.rotated(direction)
+	
 	velocity = inertia + direction_vect * speed
-
+	rotation = direction
+	
 func _physics_process(delta):
 	translate(velocity)
 	
