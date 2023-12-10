ff4f75042f7bbc5046a32edd5287c598f01620e6
adrianriverof <adrianriverof@gmail.com>
2023-12-10 23:14:29 +0100

    refactor menor: spawn

--- level.gd
+++ level.gd
@@ -5,26 +5,36 @@ onready var enemy = preload("res://enemy.tscn")
 var score = 0
 
 
+
 func numero_entre_1_y_(number):  
 	randomize()    
 	return (randi() % number + 1)
 
 
 func spawn_enemy():
+	
 	drop_enemy()
+	
 	print(score)
+	
+	check_score()
+
+func check_score():
 	if score >= 20:
 		$"you win".visible = true
 
 
 func drop_enemy():
+	
 	var enemy_instance = enemy.instance()
-	enemy_instance.position = get_node("spawn_positions").get_node(str(numero_entre_1_y_(4))).position
+	enemy_instance.position = calculate_position_to_spawn_enemy()
 	enemy_instance.player = get_node("player")
 	
 	get_tree().get_root().add_child(enemy_instance)
-	
 
+
+func calculate_position_to_spawn_enemy():
+	return get_node("spawn_positions").get_node(str(numero_entre_1_y_(4))).position
 
 func _on_enemy_spawn_timeout():
 	spawn_enemy()
