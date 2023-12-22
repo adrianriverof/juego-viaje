3c27e5c3bbb1eaadb1ae7e828a3dc7dfb0e5be22
adrianriverof <adrianriverof@gmail.com>
2023-12-22 23:07:23 +0100

    feature: different enemies get generated

--- level.gd
+++ level.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 onready var enemy = preload("res://enemy.tscn")
+onready var enemy_lateral_move = preload("res://enemy_lateral_move.tscn")
+onready var enemy_waver = preload("res://enemy_waver.tscn")
+
 
 var score = 0
 
@@ -32,11 +35,23 @@ func check_score():
 
 func drop_enemy():
 	
-	var enemy_instance = enemy.instance()
+	var enemy_instance = choose_enemy_instance() # enemy.instance()
+	
 	enemy_instance.position = calculate_position_to_spawn_enemy()
 	enemy_instance.player = get_node("player")
 	
 	get_tree().get_root().add_child(enemy_instance)
+
+func choose_enemy_instance():
+	
+	var numero_aleatorio = numero_entre_1_y_(3)
+	
+	if numero_aleatorio == 1:
+		return enemy_lateral_move.instance()
+	elif numero_aleatorio == 2:
+		return enemy_waver.instance()
+	else:
+		return enemy.instance()
 
 
 func calculate_position_to_spawn_enemy():
@@ -45,3 +60,10 @@ func calculate_position_to_spawn_enemy():
 	var posicion_aleatoria = numero_entre_1_y_(quantity_of_spawn_points)
 	
 	return spawn_node.get_node(str(posicion_aleatoria)).position
+
+
+
+
+
+
+
